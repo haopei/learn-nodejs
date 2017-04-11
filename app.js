3141bb434c35a3c23e2e402bc4afb1675af19876
Haopei Yang <haopeiyang@gmail.com>
2017-04-11 14:47:57 -0400

    Returning JSON from the server

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const http = require('http');
 const fs = require('fs')
 
 const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    const readStream = fs.createReadStream('./res.html', 'utf8');
-    readStream.pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    const data = {name: 'Haopei', age: '29' }
+    // res expects a string or a stream object,
+    // so we must stringify our data to be passed here
+    res.end(JSON.stringify(data));
 });
 
+
+// Activate server on port 3000
 server.listen('3000', '127.0.0.1');
 console.log('listening on port 3000');
