ed461ba025174d190dd5dbf85e5a6499e1b35fd1
Haopei Yang <haopeiyang@gmail.com>
2017-04-11 15:20:38 -0400

    Simple EXPRESS server

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
-const http = require('http');
-const fs = require('fs')
 
-const server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    const data = {name: 'Haopei', age: '29' }
-    // res expects a string or a stream object,
-    // so we must stringify our data to be passed here
-    res.end(JSON.stringify(data));
-});
+// returns a function
+const express = require('express');
 
+const app = express();
 
-// Activate server on port 3000
-server.listen('3000', '127.0.0.1');
-console.log('listening on port 3000');
+app.get('/', function(req, res) {
+    res.send('Hello Express');
+})
+
+app.listen(3000)
