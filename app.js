4e27e58e96652436c972ff7f9065d05482034892
Haopei Yang <haopeiyang@gmail.com>
2017-04-11 14:13:43 -0400

    Sending streams of data using .pipe()

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * Read and Write Streams
+ * Sending stream of data using pipes.
  */
 
 const fs = require('fs');
@@ -10,9 +10,10 @@ let myReadStream = fs.createReadStream(__dirname + '/read-from-me.txt', 'utf8');
 // Create WriteStream
 let myWriteStream = fs.createWriteStream(__dirname + '/write-to-me.txt');
 
-// The ReadStream object inherits the EventEmitter object.
-// It listens for the 'data' event which indicates that a new 'buffer' of data has arrived
-myReadStream.on('data', function(chunk) {
-    // write to the file one chunk at a time
-    myWriteStream.write(chunk);
-});
+// this function...
+myReadStream.pipe(myWriteStream);
+
+// ...same as this
+// myReadStream.on('data', function(chunk) {
+//     myWriteStream.write(chunk);
+// });
