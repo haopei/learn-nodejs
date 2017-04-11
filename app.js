910a1855a2c16c49fe40f44a5fc172ae7e31d613
Haopei Yang <haopeiyang@gmail.com>
2017-04-11 15:49:00 -0400

    TEMPLATING using EmbeddedJS (ejs)

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 
 // returns a function
 const express = require('express');
-
 const app = express();
+
+// let express know that we are using ejs as 'view engine'
+app.set('view engine', 'ejs');
+
+app.get('/profile/:id', function(req, res) {
+    // fetch data from datastore
+    let data = {
+        name: 'Haopei',
+        location: 404
+    }
+
+    // 'profile' corresponds to profile.ejs inside views
+    // note: 'render' instead of 'send'
+    res.render('profile', {
+        person: req.params.id,
+        data: data
+    });
+});
 
 app.get('/', function(req, res) {
     res.send('Hello Express');
 });
-
-app.get('/profile/:id', function(req, res) {
-    res.send('Profile ID: ' + req.params.id);
-})
 
 app.listen(3000);
