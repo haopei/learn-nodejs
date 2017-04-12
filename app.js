10b3583ec64c7185c1fce9bc718c1a18b608eea6
Haopei Yang <haopeiyang@gmail.com>
2017-04-11 20:27:44 -0400

    Handling post requests on EXPRESS using body-parser

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const app = express();
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+
+const urlEncodedParser = bodyParser.urlencoded({ extended: false })
+
 
 // let express know that we are using ejs as 'view engine'
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/contact', function(req, res) {
     // Visit http://127.0.0.1:3000/contact?name=haopei&age=29
     res.render('contact', { query: req.query })
-});
+})
 
-app.listen(3000);
-console.log('listening on port 3000');
+app.post('/contact', urlEncodedParser, function(req, res) {
+    // The urlEncodedParser middleware gives access to req.body
+    // which contains submitted form values
+    console.log(req.body);
+    res.render('contact-success', { data: req.body })
+})
+
+app.listen(3000)
+console.log('listening on port 3000')
